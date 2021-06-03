9c0a1a695d3e74b58e782194d9fb08285f8d76b8
David Deaton <dideater@gmail.com>
2021-06-03 08:44:27 -0700

    close to completed math

--- standardTime.js
+++ standardTime.js
@@ -1,12 +1,35 @@
 function updateTime () {
     
-    var now = moment.utc().format("dddd, MMMM Do, YYYY, h:mm:ss A");
+    var now = moment.utc().format("dddd, MMMM Do, YYYY, HH:mm:ss");
   
     $("#currentDay").text(now);
-
-    $("#currentDay2").text(now);
-
 };
 
 setInterval(updateTime, 1000);
+updateTime();
+
+function updateDeatonTime () {
+
+    var hour = moment.utc().format("HH");
+
+    var minute = moment.utc().format("mm");
+
+    var second = moment.utc().format("ss");
+
+    var deatonTime = ((hour*3600) + (minute*60) + second*1)/8640;
+
+    var deatonHour = Math.floor(deatonTime);
+
+    var deatonMin = (deatonTime - deatonHour)*100;
+
+    var deatonSec = (deatonMin - Math.floor(deatonMin))*100;
+
+    $("#currentDay2").text(deatonHour + ":" + Math.floor(deatonMin) + ":" + Math.round(deatonSec));
+
+
+};
+
+setInterval(updateDeatonTime, 12);
+setInterval(updateTime, 1000);
+updateDeatonTime ();
 updateTime();
